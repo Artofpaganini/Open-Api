0735be96e665a27ab23a44f4b8529ce90d197645
artofpaganini@gmail.com <Grozbeckert2015>
2021-03-07 18:07:18 +0300

    added retrofit

--- app/src/main/java/com/example/open_api/api/auth/OpenApiAuthService.kt
+++ app/src/main/java/com/example/open_api/api/auth/OpenApiAuthService.kt
@@ -1,4 +1,27 @@
 package com.example.open_api.api.auth
 
-class OpenApiAuthService {
+import androidx.lifecycle.LiveData
+import com.example.open_api.api.auth.network_responses.LoginResponse
+import com.example.open_api.api.auth.network_responses.RegistrationResponse
+import com.example.open_api.util.GenericApiResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface OpenApiAuthService {
+    @POST("account/login")
+    @FormUrlEncoded
+    fun login(
+        @Field("username") email: String,
+        @Field("password") password: String
+    ): LiveData<GenericApiResponse<LoginResponse>>
+
+    @POST("account/register")
+    @FormUrlEncoded
+    fun register(
+        @Field("email") email: String,
+        @Field("username") username: String,
+        @Field("password") password: String,
+        @Field("password2") password2: String
+    ): LiveData<GenericApiResponse<RegistrationResponse>>
 }
--- app/src/main/java/com/example/open_api/di/AppModule.kt
+++ app/src/main/java/com/example/open_api/di/AppModule.kt
@@ -10,12 +10,33 @@ import com.example.open_api.persistence.AccountPropertiesDao
 import com.example.open_api.persistence.AppDatabase
 import com.example.open_api.persistence.AppDatabase.Companion.DATABASE_NAME
 import com.example.open_api.persistence.AuthTokenDao
+import com.example.open_api.util.Constants
+import com.example.open_api.util.LiveDataCallAdapterFactory
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 class AppModule{
+
+    @Singleton
+    @Provides
+    fun provideGsonBuilder(): Gson {
+        return GsonBuilder().excludeFieldsWithoutExposeAnnotation().create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitBuilder(gsonBuilder: Gson): Retrofit.Builder{
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addCallAdapterFactory(LiveDataCallAdapterFactory())
+            .addConverterFactory(GsonConverterFactory.create(gsonBuilder))
+    }
 
     @Singleton
     @Provides
